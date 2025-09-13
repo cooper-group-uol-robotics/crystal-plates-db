030b1fd21ed5ca4dab099bc3a54d140ff4cb64af
Thomas Fellowes <thomas.fellowes@liverpool.ac.uk>
2025-09-13 16:21:39 +0100

    Update segmentation API endpoint configuration: Change default URL to new endpoint

--- config/initializers/segmentation.rb
+++ config/initializers/segmentation.rb
@@ -1,7 +1,6 @@
 # Configuration for image segmentation API
 Rails.application.configure do
   # URL for the external segmentation API
-  # Default: http://aicdocker.liv.ac.uk:8000/segment/opencv
   # Can be overridden with SEGMENTATION_API_ENDPOINT environment variable
-  config.segmentation_api_endpoint = ENV.fetch("SEGMENTATION_API_ENDPOINT", "http://aicdocker.liv.ac.uk:8000/segment/opencv")
+  config.segmentation_api_endpoint = ENV.fetch("SEGMENTATION_API_ENDPOINT", "http://10.10.1.100:8000/segment/opencv")
 end
