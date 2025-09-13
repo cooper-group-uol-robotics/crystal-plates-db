938c9e47932fab2f89377a07aab229821170a6e1
Thomas Fellowes <thomas.fellowes@liverpool.ac.uk>
2025-09-13 16:35:11 +0100

    Update segmentation API endpoint: Change default URL to new endpoint

--- config/initializers/segmentation.rb
+++ config/initializers/segmentation.rb
@@ -2,5 +2,5 @@
 Rails.application.configure do
   # URL for the external segmentation API
   # Can be overridden with SEGMENTATION_API_ENDPOINT environment variable
-  config.segmentation_api_endpoint = ENV.fetch("SEGMENTATION_API_ENDPOINT", "http://10.10.1.100:8000/segment/opencv")
+  config.segmentation_api_endpoint = ENV.fetch("SEGMENTATION_API_ENDPOINT", "http://10.10.1.100:8000/segment/sam")
 end
