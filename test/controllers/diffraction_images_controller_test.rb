00b18f95e69662ecf7bf45ceea16c1682d614c81
Tom Fellowes <thomas.fellowes@liverpool.ac.uk>
2025-09-23 09:42:24 +0100

    Refactor diffraction images tests and update fixture data for accuracy

--- test/controllers/diffraction_images_controller_test.rb
+++ test/controllers/diffraction_images_controller_test.rb
@@ -1,23 +1,38 @@
 require "test_helper"
 
 class DiffractionImagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @scxrd_dataset = scxrd_datasets(:one)
+    @diffraction_image = diffraction_images(:one)
+  end
+
   test "should get index" do
-    get diffraction_images_index_url
+    get scxrd_dataset_diffraction_images_url(@scxrd_dataset)
     assert_response :success
   end
 
   test "should get show" do
-    get diffraction_images_show_url
+    get scxrd_dataset_diffraction_image_url(@scxrd_dataset, @diffraction_image)
     assert_response :success
   end
 
-  test "should get image_data" do
-    get diffraction_images_image_data_url
-    assert_response :success
+  test "should handle image_data without attached file" do
+    get image_data_scxrd_dataset_diffraction_image_url(@scxrd_dataset, @diffraction_image), 
+        as: :json
+    assert_response :unprocessable_entity
+    
+    response_data = JSON.parse(response.body)
+    assert_equal false, response_data["success"]
+    assert_equal "No rodhypix file attached", response_data["error"]
   end
 
-  test "should get download" do
-    get diffraction_images_download_url
-    assert_response :success
+  test "should handle download without attached file" do
+    get download_scxrd_dataset_diffraction_image_url(@scxrd_dataset, @diffraction_image),
+        as: :json
+    assert_response :unprocessable_entity
+    
+    response_data = JSON.parse(response.body)
+    assert_equal false, response_data["success"]
+    assert_equal "No rodhypix file attached", response_data["error"]
   end
 end
