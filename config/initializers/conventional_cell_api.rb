ea1c6560362ece1beb9ffafda294f2bb09f4777a
Thomas Fellowes <thomas.fellowes@liverpool.ac.uk>
2025-10-02 23:16:17 +0100

    fix: Update conventional cell API base URL to correct endpoint

--- config/initializers/conventional_cell_api.rb
+++ config/initializers/conventional_cell_api.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.conventional_cell_api_enabled = ENV.fetch("CONVENTIONAL_CELL_API_ENABLED", "true").downcase == "true"
 
   # Unit cell conversion API base URL
-  config.conventional_cell_api_base_url = ENV.fetch("CONVENTIONAL_CELL_API_BASE_URL", "http://localhost:8000")
+  config.conventional_cell_api_base_url = ENV.fetch("CONVENTIONAL_CELL_API_BASE_URL", "http://localhost:3001")
 
   # Default maximum delta for unit cell conversion tolerance
   config.conventional_cell_max_delta = ENV.fetch("CONVENTIONAL_CELL_MAX_DELTA", "1.0").to_f
