f91a19483137ad1fcae56c7ee042bc3aa2006ec6
Tom Fellowes <fellowes@liverpool.ac.uk>
2025-11-13 09:52:51 +0000

    Remove custom attributes association from Plate model

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -6,7 +6,6 @@ class Plate < ApplicationRecord
     has_many :plate_locations
     has_many :locations, through: :plate_locations
     has_many :calorimetry_experiments, dependent: :destroy
-    has_many :custom_attributes, as: :attributable, dependent: :destroy
     has_many :well_scores, through: :wells
 
     validates :barcode, uniqueness: true
